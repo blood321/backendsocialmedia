9bd9c8ae0be660f030f14d581f835b69921ca80b
wendisay28 <wendy.nietov@gmail.com>
2021-11-25 18:49:57 -0500

    actualizaciones

--- index.js
+++ index.js
@@ -4,9 +4,13 @@ const mongoose = require("mongoose");
 const dotenv = require("dotenv");
 const helmet = require("helmet");
 const morgan = require("morgan");
+const multer = require("multer");
 const userRoute = require("./routes/users");
 const authRoute = require("./routes/auth");
 const postRoute = require("./routes/posts");
+const router = express.Router();
+
+const path = require("path");
 
 
 dotenv.config();
@@ -18,11 +22,31 @@ mongoose.connect(
     console.log("Connected to MongoDB");
   }
 );
+app.use("/images", express.static(path.join(__dirname, "public/images")));
+
 
 //middleware
 app.use(express.json());
 app.use(helmet());
 app.use(morgan("common"));
+
+const storage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, "public/images");
+  },
+  filename: (req, file, cb) => {
+    cb(null, req.body.name);
+  },
+});
+
+const upload = multer({ storage: storage });
+app.post("/api/upload", upload.single("file"), (req, res) => {
+  try {
+    return res.status(200).json("File uploded successfully");
+  } catch (error) {
+    console.error(error);
+  }
+});
 
 
 app.use("/api/users", userRoute);
